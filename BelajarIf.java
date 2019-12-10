5b8ca076dda0a93c818d3063733583ea0d3e35ce
Ilham Dwi Nugraha <ilhamdn99@gmail.com>
2019-12-10 10:37:42 +0700

    Belajar Percabangan If (If saja)

---
+++ BelajarIf.java
@@ -0,0 +1,8 @@
+public class BelajarIf{
+    public static void main(String[] args) {
+        boolean hujan = true;
+        if(hujan==true){
+            System.out.println("Bawa Payung");
+        }
+    }
+}
